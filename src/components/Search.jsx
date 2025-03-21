8ada33709fe06108c05b5f5e29e0f1e72b10aebb
maomao0007 <skylifepic07@gmail.com>
2025-03-22 01:20:58 +0800

    feat: add infinite scroll feature

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { Heart } from "lucide-react";
@@ -8,42 +8,72 @@ const Search = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [photos, setPhotos] = useState([]);
   const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [page, setPage] = useState(1);
   const { lovedPhotos, toggleLove } = useLoved();
+  const isLoading = useRef(false);
+  const currentPage = useRef(1);
+  const listRef = useRef(null);
+  const timerRef = useRef(null);   
   const apiKey = import.meta.env.VITE_PHOTO_API_KEY;
 
-  const fetchPhotos = async (term, currentPage) => {
+  const fetchPhotos = async (term, page = 1) => {
     try {
+      isLoading.current = true;
+
       const url = term
-        ? `https://api.pexels.com/v1/search?query=${term}&per_page=30&page=${currentPage}`
-        : `https://api.pexels.com/v1/curated?page=${currentPage}&per_page=30`;
+        ? `https://api.pexels.com/v1/search?query=${term}&per_page=50&page=${page}`
+        : `https://api.pexels.com/v1/curated?per_page=50&page=${page}`;
 
       const response = await axios.get(url, {
         headers: { Authorization: apiKey },
       });
-      setPhotos(response.data.photos);
+      setPhotos((prePhoto) => {
+        return [...prePhoto, ...response.data.photos];
+      });
+
       setError(null);
-      setLoading(false);
+      isLoading.current = false;
+
     } catch (err) {
       setError(err.message);
-      setLoading(false);
+      isLoading.current = false;
     }
   };
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      fetchPhotos(searchTerm, page);
-    }, 500)
-    
-    return () => clearTimeout(timer);
-  }, [searchTerm, page, apiKey]);
+     if (timerRef.current) {
+       clearTimeout(timerRef.current);
+     }
+
+    timerRef.current = setTimeout(() => {
+      setPhotos([]);
+      currentPage.current = 1;
+      fetchPhotos(searchTerm, 1);
+    }, 500);
+
+    return () => {
+      if (timerRef.current) {
+        clearTimeout(timerRef.current);
+      }
+    };
+  }, [searchTerm]);
 
   useEffect(() => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  }, [page]);
+    const handleScroll = () => {
+      const height =
+        listRef.current.offsetHeight +
+        listRef.current.offsetTop -
+        window.innerHeight;
+      if (!isLoading.current && window.scrollY > height) {
+        currentPage.current++;
+        fetchPhotos(searchTerm, currentPage.current);
+      }
+    };
 
-  if (loading) {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [searchTerm]);
+
+  if (isLoading.current) {
     return (
       <div className="min-h-screen w-full flex items-center justify-center bg-gray-100">
         <div className="text-center">
@@ -55,28 +85,20 @@ const Search = () => {
     );
   }
 
-  const handlePrevPage = () => {
-    if (page > 1) {
-      setPage((prev) => prev - 1);
-    }
-  };
-
-  const handleNextPage = () => {
-    setPage((prev) => prev + 1);
-  };
-
   const handleSearch = (e) => {
     setSearchTerm(e.target.value);
-    setPage(1);
   };
 
-  const renderPhotoItem = (photo) => (
-    <Link to={`/photos/${photo.id}`} key={photo.id}>
+  const renderPhotoItem = (photo, index) => (
+    <Link to={`/photos/${photo.id}`} key={`${photo.id}-${index}`}>
       <div className="relative rounded-lg overflow-hidden shadow-md transition-transform duration-300 hover:scale-[1.03] hover:shadow-lg cursor-pointer aspect-4/3 h-64">
         <Heart
           size={24}
           fill={photo.id in lovedPhotos ? "pink" : "none"}
-          onClick={(e) => toggleLove(photo, e)}
+          onClick={(e) => {
+            e.preventDefault();
+            toggleLove(photo, e);
+          }}
           className="absolute top-4 right-4 z-10 transform hover:scale-110 transition-transform"
         />
         <img
@@ -102,27 +124,14 @@ const Search = () => {
         className="w-full px-4 py-2 mb-6 border border-gray-300 text-gray-700 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
       />
 
-      {error && <div className="text-red-500 mb-4">{error}</div>}
+      {error && (
+        <div className="mb-6 rounded-md bg-red-100 px-4 py-2 text-red-600">
+          {error}
+        </div>
+      )}
 
-      <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
+      <div ref={listRef} className="grid grid-cols-1 sm:grid-cols-3 gap-4">
         {(searchTerm === "" ? photos : filterPhotos).map(renderPhotoItem)}
-      </div>
-
-      <div className="flex items-center justify-center gap-4 mt-8">
-        <button
-          onClick={handlePrevPage}
-          disabled={page <= 1}
-          className="px-4 py-2 bg-slate-500 text-white rounded-lg disabled:opacity-50 disabled:cursor-not-allowed    hover:bg-slate-600"
-        >
-          Previous
-        </button>
-        <span className="text-slate-700">Page {page}</span>
-        <button
-          onClick={handleNextPage}
-          className="px-4 py-2 bg-slate-500 text-white rounded-lg     hover:bg-slate-600"
-        >
-          Next
-        </button>
       </div>
     </div>
   );
